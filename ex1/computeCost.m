9b8689fa3dd2d645c0139ad460151e613bd10c78
Youngjae Hur <youngjae1994@utexas.edu>
2020-09-16 13:54:52 +0000

    Add computing cost function

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -5,6 +5,8 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+predictions = X * theta;
+
 
 % You need to return the following variables correctly 
 J = 0;
@@ -12,7 +14,7 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+J = 1/(2*m) * sum((predictions-y).^2);
 
 
 
