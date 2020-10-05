cd98067797aa7dff62b5074e1c05809bb55751c6
Youngjae Hur <youngjae1994@utexas.edu>
2020-10-05 12:42:05 +0000

    Add predict of all

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,9 +21,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(size(z2), 1) sigmoid(z2)];
 
+z3 = a2 * Theta2';
+pred = sigmoid(z3);
 
-
+[probability, index_max] = max(pred, [], 2);
+p = index_max;
 
 
 
