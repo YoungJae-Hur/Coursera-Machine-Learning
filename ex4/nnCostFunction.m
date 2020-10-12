40731d442fca6214fad6c37241cf6501970d3b36
Youngjae Hur <youngjae1994@utexas.edu>
2020-10-12 15:00:00 +0000

    Add nnCost function part 1

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -61,6 +61,17 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+    
+% part 1
+a1 = [ones(m, 1), X];
+z2 = a1 * Theta1';
+% a2 = sigmoid(z2);
+a2 = [ones(m, 1), sigmoid(z2)];
+z3 = a2 * Theta2';
+hypothesis = sigmoid(z3);
+
+y = repmat([1:num_labels], m, 1) == repmat(y, 1, num_labels);
+J = (-1/m)*sum(sum(y.*log(hypothesis) + (1-y).*log(1-hypothesis)));
 
 
 
