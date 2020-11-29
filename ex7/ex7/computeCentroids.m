0478a18c794966a34a80375d78684586a495208d
Youngjae Hur <youngjae1994@utexas.edu>
2020-11-29 13:54:37 +0000

    Finalize computing centroids

--- ex7/ex7/computeCentroids.m
+++ ex7/ex7/computeCentroids.m
@@ -26,7 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for k = 1:K,
+   k_idx = (idx == k);
+   centroids(k,:) = ((k_idx' * X) ./ sum(k_idx));
+end
 
 
 
